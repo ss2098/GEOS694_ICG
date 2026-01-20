f5a4110ea70a20e01a101f62103a3a4ea9735ee9
ss2098 <sshukla2@alaska.edu>
2026-01-20 11:05:03 -0900

    Updated GEOS694_Shukla_HW1.py with my latest edits

--- HW1/GEOS694_Shukla_HW1.py
+++ HW1/GEOS694_Shukla_HW1.py
@@ -1,14 +1,2 @@
-#import numpy as np
-#print(np.random.choice(np.arange(1, 11), size=10, replace=False))
 
-#main code 
-import numpy as np
-
-num = int(np.random.random() * 10) + 1
-print(num)
-#array from 1 to 10 randomly shuffled
-arr=np.arange(1, 11)
-np.random.shuffle(arr) 
-print(arr)
-result = arr[:num]
-print(result)
+python3 -c 'print ("Hello, World!")'
